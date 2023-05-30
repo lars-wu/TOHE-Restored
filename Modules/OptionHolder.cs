8a6edf9446edc2cc31c68c272f7c1910e6d58d29
lars-wu <papercut92@hotmail.de>
2023-05-30 19:25:58 +0200

    Soul Catcher

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -661,6 +661,7 @@ public static class Options
             .SetValueFormat(OptionFormat.Seconds);
         ImperiusCurseShapeshiftCooldown = FloatOptionItem.Create(902435, "ShapeshiftCooldown", new(1f, 999f, 1f), 15f, TabGroup.ImpostorRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.ImperiusCurse])
             .SetValueFormat(OptionFormat.Seconds);
+        ImperiusCurse.SetupCustomOption();
         QuickShooter.SetupCustomOption();
         Hangman.SetupCustomOption();
 
